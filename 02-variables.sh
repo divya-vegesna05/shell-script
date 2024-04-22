c31d326ca8788128fe9fc82dc6474ce850dfeec2
divya <divya.vegesna55@gmail.com>
2024-04-22 12:06:12 +0530

    using variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Ramesh: hello Suresh,good morning"
-echo "Suresh: Hi Ramesh,very good morning"
-echo "Ramesh: How are you Suresh?"
-echo "Suresh: I amgood Ramesh,How are you?"
+person1=Ramesh
+person2=Suresh
+echo "$person1: hello $person2,good morning"
+echo "$person2: Hi $person1,very good morning"
+echo "$person1: How are you $person2?"
+echo "$person2: I am good $person1,How are you?"
