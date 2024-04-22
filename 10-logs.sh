7252b6d4ebb54b4d1e2ee57b06570f00f0ffbb5a
divya <divya.vegesna55@gmail.com>
2024-04-22 19:59:08 +0530

    install mysql

--- 10-logs.sh
+++ 10-logs.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 ID=$(id -u)
+RED="\033[31m"
+GREEN="\033[32m"
+NORMAL="\033[0m"
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOGFILE=/tmp/$0_$TIMESTAMP.log
+echo "current time is $TIMESTAMP"
 VALIDATE(){
 if [ $1 -ne 0 ]
 then
-echo "Error: $2 failed"
+echo "Error: $2 $RED failed "
 else
-echo "$2 success"
+echo "$2 $GREEN success"
 fi
 }
 if [ $ID -ne 0 ]
 then
-echo "not a root user"
+echo "$RED ERROR: not a root user"
 exit 1
 else
 echo "root user"
