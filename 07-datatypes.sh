3690c90b74311235fe98232643e04b4e30030782
divya <divya.vegesna55@gmail.com>
2024-04-22 13:11:46 +0530

    using arguments

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -4,4 +4,4 @@ echo "fruit name 1 is ${fruits[0]}
 echo "fruit name 2 is ${fruits[1]}
 echo "fruit name 3 is ${fruits[2]}
 echo "fruit names is ${fruits[@]}
-echo "lenght is $#(fruits)"
+echo "lenght is $(#{fruits})"
