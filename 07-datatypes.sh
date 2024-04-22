23269d2a9efe358d5446a6a4ac69c2f804b5e231
divya <divya.vegesna55@gmail.com>
2024-04-22 13:07:39 +0530

    using arguments

---
+++ 07-datatypes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+fruits=("apple" "banana" "mango")
+echo "fruit name 1 is ${fruits[0]}
+echo "fruit name 2 is ${fruits[1]}
+echo "fruit name 3 is ${fruits[2]}
+echo "fruit names is ${fruits[@]}
+echo "lenght is ${#(fruits)}
