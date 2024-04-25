680e427416c734e6f647ccc91a2b63a946c050d7
divya <divya.vegesna55@gmail.com>
2024-04-25 22:12:52 +0530

    install mysql

--- 12-removelogs.sh
+++ 12-removelogs.sh
@@ -7,10 +7,11 @@ NORMAL="\e[0m"
 if [ ! -d $SOURCE_DIR ]
 then
 echo -e "$RED ERROR: $SOURCE_DIR does not exists"
-exit 1
 fi
-FILES_TO_DELETE= $(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
 while IFS= read -r line
 do 
-echo "deleting line $line"
+   echo "deleting line $line"
 done <<< $FILES_TO_DELETE
