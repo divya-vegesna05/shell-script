049f43f16ba67947d508f45148e8fdb90575c2ae
divya <divya.vegesna55@gmail.com>
2024-04-25 21:37:56 +0530

    install mysql

---
+++ 12-removelogs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SOURCE_DIR="/tmp/shellscript-logs"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+NORMAL="\e[0m"
+if [ ! -d $SOURCE_DIR ]
+then
+echo -e "$RED ERROR: $SOURCE_DIR does not exists"
+exit 1
+fi
+FILES_TO_DELETE= $(find $SOURCE_DIR -type -f -mtime +14 -name  "*.log")
+while IFS= read -r line
+do 
+echo "deleting line $line"
+done <<< $FILES_TO_DELETE
