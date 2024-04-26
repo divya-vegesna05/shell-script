e99e491f4887df6a11edebea402aaaa7400d8761
divya <divya.vegesna55@gmail.com>
2024-04-26 20:08:24 +0530

    ifs

--- roboshop.sh
+++ roboshop.sh
@@ -3,13 +3,13 @@ AMI_IMAGE="ami-0f3c7d07486cad139"
 SG_ID="sg-0e0ea845438880613"
 SUBNET_ID="subnet-042abd3162d58609f"
 INSTANCES=("web","cart","catalogue","shipping","user","mysql")
-for i in ${INSTANCES[@]}
+for i in "${INSTANCES[@]}"
 do
-if [ $i=="mysql" || $i=="shipping" ];
+if [ $i=="mysql" ] || [ $i=="shipping" ]
 then
 INSTANCE_TYPE="t3.small"
 else
 INSTANCE_TYPE="t2.micro"
 fi
-aws ec2 run-instances --image-id $AMI_IMAGE --count 1 --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID  --subnet-id $SUBNET_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=name,Value=$i}]'
+aws ec2 run-instances --image-id $AMI_IMAGE --count 1 --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID  --subnet-id $SUBNET_ID --tag-specifications --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]'
 done
