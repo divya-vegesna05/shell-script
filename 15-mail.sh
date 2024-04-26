5b6cb8e3f01d615335dfd2091738330c37c597a2
divya <divya.vegesna55@gmail.com>
2024-04-26 16:58:48 +0530

    ifs

--- 15-mail.sh
+++ 15-mail.sh
@@ -4,5 +4,5 @@ ALERT_TYPE=$2
 BODY=$3
 SUBJECT=$4
 TO_ADDRESS=$5
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$BODY/g" template.html)
+FINAL_BODY=$(sed -e 's/TO_TEAM/$TO_TEAM/g' -e 's/ALERT_TYPE/$ALERT_TYPE/g' -e 's/BODY/$BODY/g' template.html)
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT \n Content-Type: text/html")" "$TO_ADDRESS"
