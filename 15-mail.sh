6244c2c02339ace103800bd17d5753ff97fffb6b
divya <divya.vegesna55@gmail.com>
2024-04-26 17:10:06 +0530

    ifs

--- 15-mail.sh
+++ 15-mail.sh
@@ -2,7 +2,8 @@
 TO_TEAM=$1
 ALERT_TYPE=$2
 BODY=$3
+ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 SUBJECT=$4
 TO_ADDRESS=$5
-FINAL_BODY=$(sed -e 's/TO_TEAM/$TO_TEAM/g' -e 's/ALERT_TYPE/$ALERT_TYPE/g' -e 's/BODY/$BODY/g' template.html)
-echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT \n Content-Type: text/html")" "$TO_ADDRESS"
+FINAL_BODY=$(sed -e 's/TO_TEAM/$TO_TEAM/g' -e 's/ALERT_TYPE/$ALERT_TYPE/g' -e 's/BODY/$ESCAPE_BODY/g' template.html)
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
