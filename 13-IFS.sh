ea626cdeff25e674af1c89f25f1d74b0706c1343
divya <divya.vegesna55@gmail.com>
2024-04-26 14:35:18 +0530

    ifs

---
+++ 13-IFS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SOURCE_DIR="/etc/passwd"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+NORMAL="\e[0m"
+if [ ! -d $SOURCE_DIR ]
+then
+echo "$RED ERROR: $SOURCE_DIR doesnot exist $NORMAL"
+fi
+while IFS=":" read -r username password userid groupid userid_info homedir command
+do
+echo "username is $username"
+echo "userid is $userid"
+echo "homedir is $homedir"
+done < $SOURCE_DIR
