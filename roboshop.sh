911a0bb98ed87a9581e7c558f7e83b376cc0195c
divya <divya.vegesna55@gmail.com>
2024-04-26 21:00:22 +0530

    ifs

--- roboshop.sh
+++ roboshop.sh
@@ -3,6 +3,8 @@ AMI_IMAGE=ami-0f3c7d07486cad139
 SG_ID=sg-0e0ea845438880613
 SUBNET_ID="subnet-042abd3162d58609f"
 INSTANCES=("web" "cart" "catalogue" "shipping" "user" "mysql")
+ZONE_ID=Z07231219S408J0QGA3I
+DOMAIN_NAME="jasritha.tech"
 for i in "${INSTANCES[@]}"
 do
 if [ $i == "mysql" ] || [ $i == "shipping" ]
@@ -13,4 +15,25 @@ else
 fi
 PrivateIpAddress=$(aws ec2 run-instances --image-id $AMI_IMAGE --count 1 --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 echo $i:$PrivateIpAddress
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file://<(cat << EOF
+{
+  "Comment": "Testing creating a record set",
+  "Changes": [
+    {
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "${i}.$DOMAIN_NAME",
+        "Type": "CNAME",
+        "TTL": 1,
+        "ResourceRecords": [
+          {
+            "Value": "${PrivateIpAddress}"
+          }
+        ]
+      }
+    }
+  ]
+}
+EOF
+)
 done
