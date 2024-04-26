f07cab26626bdd528aeb4611dfca6677dc80f189
divya <divya.vegesna55@gmail.com>
2024-04-26 19:54:34 +0530

    ifs

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@ AMI_IMAGE="ami-0f3c7d07486cad139"
 SG_ID="sg-0e0ea845438880613"
 SUBNET_ID="subnet-042abd3162d58609f"
 INSTANCES=("web","cart","catalogue","shipping","user","mysql")
-for i in INSTANCES[@]
+for i in ${INSTANCES[@]}
 do
 if [ $i == "mysql" || $i == "shipping"]
 then
