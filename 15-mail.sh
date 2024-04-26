e96f54c8034c160bee1efda929840e758f52b6ef
divya <divya.vegesna55@gmail.com>
2024-04-26 17:27:06 +0530

    ifs

--- 15-mail.sh
+++ 15-mail.sh
@@ -6,4 +6,4 @@ ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 SUBJECT=$4
 TO_ADDRESS=$5
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
-echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
+echo -e "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
